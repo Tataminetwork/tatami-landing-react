7bacc62877e0616f3f14478e98ffd0b25e6c99ba
Betrin13 <betrin13@gmail.com>
2017-10-05 16:00:35 +0100

    Add data and images

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
-import map from '../assets/images/map.png';
+// import map from '../../public/images/map.png';
+import Data from '../data.json'
 import Person from './Person';
 
 export default class Map extends Component {
   
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      people: Data.people,
+      content: Data.content,
+    };
+    console.log('constructor');
+    console.log(this.state.people);
   }
 
   render() {
 
-    const { persons } = this.state;
+    const { people } = this.state;
+    
+    const PeopleComponents = people.map((person) => {
+      return <img key={person.avatar} src={person.avatar}/>;
+    });
 
     return (
       <div className="container">
         <div className="row">
           <div className="col-md-12 map">
+            {PeopleComponents}
           </div>
         </div>
     </div>
